b9b975d595e3868c16e4cf3aa67263369f870962
kajela <titoyut56@gmail.com>
2023-04-16 05:04:41 -0400

    add the initial files

--- index.js
+++ index.js
@@ -2,4 +2,24 @@
 let employee = {
     name: "sam",
     streetAddress: "112541",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]: value};
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {employee};
+    delete newEmployee[key];
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
 }
